95040dc66898880eff37c6a5efb22154a2418aa8
KarlKarli <134058251+KarlKarli@users.noreply.github.com>
2023-05-20 19:39:47 +0800

    Add files via upload

---
+++ SpaceXjs.js
@@ -0,0 +1,15 @@
+var arr = ["Fact #1 The Dragon spacecraft is capable of carrying up to 7 passengers to and from Earth orbit, and beyond. It is the only spacecraft currently flying that is capable of returning significant amounts of cargo to Earth, and is the first private spacecraft to take humans to the space station.", 
+		"Fact #2 SpaceX's billionaire CEO Elon Musk has said that he named his spacecraft 'Dragon' after the fictional 'Puff the Magic Dragon,' from the hit song by music group Peter, Paul and Mary. Musk said he used the name because many critics considered his goals impossible when he founded SpaceX in 2002.",
+		"Fact #3 On the Dragon capsule's ( the first private spacecraft to take humans to the space station ) first test flight in December 2010, it carried a wheel of cheese into orbit. The wheel of cheese was launched in honor of a classic skit from actor John Cleese in the British comedy show Monty Python's Flying Circus.",
+		"Fact #4 SpaceX's fleet of Falcon rockets — the Falcon 9 boosters and smaller Falcon 1 rockets — are named after the fictional Millenium Falcon spaceship from creator George Lucas' 'Star Wars' movies.",
+		"Fact #5 Part of the live-action superhero movie 'Iron Man 2' was shot in SpaceX's factory in Hawthorne, Calif., and CEO Elon Musk even graced the silver screen with a cameo as himself.",
+		"Fact #6 Musk, who founded SpaceX in 2002, is also the CEO of Tesla Motors and made his initial fortune by co-founding the online payment system PayPal. The entrepreneur has also developed a larger-than-life persona, with admirers calling him the real-life Tony Stark.",
+		"Fact #7 One of SpaceX's rockets, the Falcon Heavy, is as of today the most powerful operational rockets in the world.",
+		"Fact #8 SpaceX's Starship is an in-development, fully reusable, super-heavy-lift launch vehicle and second stage of the Starship system. While still in development, when complete it will be the tallest and most powerful launch vehicle ever built.",
+		"Fact #9 Starlink is a satellite internet constellation operated by SpaceX. The constellation currently comprises more than 3,000 mass-produced tiny satellites, as of September 2022. These satellites connect with specialized ground transceivers.",
+		"Fact #10 SpaceX is funded by the U.S. government (National Aeronautics and Space Administration) and by institutional investors."];
+	
+function Function() {
+	var input = document.getElementById('input').value;
+	alert(arr[input-1]);
+}
